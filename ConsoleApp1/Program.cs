0442f7378ffdb71bca765f6ba0f67faaf80df4c0
Piotr Nowak <peterek99914@interia.pl>
2021-04-16 17:08:15 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ConsoleApp1
+{    class student
+    {
+        public int ID { set; get; }
+        public string Imie { set; get; }
+        public string Nazwisko { set; get; }
+    }
+    class Studenty : DbContext
+    {
+        public virtual DbSet<student> studenty { set; get; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baza = new Studenty();
+            student s1 = new student { Imie = "Adam", Nazwisko = "Kowal" };         
+            student s2 = new student { Imie = "Ewa", Nazwisko = "Nowak" };
+            baza.studenty.Add(s1);
+            baza.studenty.Add(s2);
+            baza.SaveChanges();
+            var war = (from v in baza.studenty select v).ToList<student>();
+            foreach (var x in war)
+            {
+                Console.Write("ID: "+x.ID.ToString());
+                Console.Write(" Imie: "+x.Imie);
+                Console.Write(" Nazwisko: " + x.Nazwisko);
+            }
+            Console.Read();
+        }
+    }
+}
